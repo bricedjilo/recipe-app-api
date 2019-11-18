51fa128ecae93f5af52a5b613b661a09bcfa5602
Brice Djilo <brice.djilo@endurance.com>
2019-11-18 16:20:15 -0700

    Correct linting errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,7 +12,6 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -20,20 +19,18 @@ class ModelTests(TestCase):
         """Test the email for the new user is normalized"""
         email = 'test@LONDONAPPDEV.COM'
         user = get_user_model().objects.create_user(email, 'test123')
-
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test123')
-    
+
     def test_create_new_super_user(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
             'test@londonappdev.cpom',
             'test123'
         )
-
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
